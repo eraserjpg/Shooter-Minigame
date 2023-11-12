864a54c34e91e0eea67db32171ccf16d3e49f9b1
seangenutis <seangenutis@gmail.com>
2023-11-12 16:11:02 -0500

    PowerUp_Health
    -made heart move randomly
    -made it so game detects when player
    collides with powerup

---
+++ Assets/Scripts/PowerUp_Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Health : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 6);
+        if (transform.position.x > 25f)
+        {
+            transform.position = new Vector3(Random.Range(-25f, -35f), Random.Range(0.5f, -3.5f), 0);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log ("Power up picked up!");
+    }
+
+}
+
